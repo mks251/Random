5e51b2b987e550b936e538eb925af33d3edc0d0f
User <User@192.168.0.102>
2016-02-07 20:25:59 +0600

    Operatcii c randomnimi chislami

---
+++ Tutorel17/src/ru/t17/muhov/Main.java
@@ -0,0 +1,21 @@
+package ru.t17.muhov;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+private static ArrayList<Mobile> mobiles = new ArrayList<Mobile>();
+private static Random r = new Random();
+
+public static void main(String[] args){
+	for(int i=0;i<300;i++){
+		mobiles.add(new Mobile(r.nextInt(9999999), "PRIRODA"));
+	}
+	for(Mobile m: mobiles){
+		System.out.println(m.getNumber()+" "+m.getName());
+	}
+	}
+	
+	}
+
+
---
+++ Tutorel17/src/ru/t17/muhov/Mobile.java
@@ -0,0 +1,23 @@
+package ru.t17.muhov;
+
+public class Mobile {
+private int number;
+private String name;
+public Mobile(int number, String name){
+	this.name = name;
+	this.number = number;
+	
+}
+public int getNumber() {
+	return number;
+}
+public void setNumber(int number) {
+	this.number = number;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+}
